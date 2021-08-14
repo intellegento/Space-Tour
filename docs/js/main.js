6e15222429f5f2d6219183ea85c74d9b988a15d6
intellegento <61728197+intellegento@users.noreply.github.com>
2021-08-14 21:57:49 +0600

    finish build layout

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-
+$(function () {
+  $(".slider-box").slick({
+    prevArrow:
+      '<img class="slider-arrow slider-arrow-left" src="images/arrow-left.png" alt="arrow-left">',
+    nextArrow:
+      '<img class="slider-arrow slider-arrow-right" src="images/arrow-right.png" alt="arrow-right">',
+  });
 });
